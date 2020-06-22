686e8b7b1530b0105a03e8d603f3c9dd9901260e
Bennyblasco <bennyblasco@gmail.com>
2020-06-22 23:14:07 +0100

    New Model

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
+ 
