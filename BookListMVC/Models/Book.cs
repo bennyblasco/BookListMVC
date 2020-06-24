618861fbe3746e4dde25c0d46aa0f7e4820d81f5
Bennyblasco <bennyblasco@gmail.com>
2020-06-25 00:45:15 +0100

    Finished setting up Database

--- BookListMVC/Models/Book.cs
+++ BookListMVC/Models/Book.cs
@@ -7,11 +7,10 @@ namespace BookListMVC.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
-        [DataType(DataType.Date)]
         public string Name { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public decimal Price { get; set; }
 
