abb840ae7ec57754507f7140a769bf3e889767d3
Bennyblasco <bennyblasco@gmail.com>
2020-06-24 22:01:11 +0100

    Changed models

--- BookListMVC/Models/Book.cs
+++ BookListMVC/Models/Book.cs
@@ -5,14 +5,17 @@ namespace BookListMVC.Models
 {
     public class Book
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public string Name { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
-        public string MyProperty { get; set; }
-
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
 
     }
 }
+
+
